c14e2a26d9367e423a3de311e2dc8d10ca32c787
Dennis-devs <dennis.ogoll@gmail.com>
2023-06-13 14:30:07 +0300

    Edit APP

--- src/App.jsx
+++ src/App.jsx
@@ -37,15 +37,15 @@ function App() {
   return(
   <div id="drum-machine">
     <div className="inner1">
-      <button className="drum-pad" id="Heater 1">Q<audio data-keys={81} src="../public/Heater-1.mp3" className="clip" id="Q"></audio></button>
-      <button className="drum-pad" id="Heater 2">W<audio data-keys={87} src="../public/Heater-2.mp3" className="clip" id="W"></audio></button>
-      <button className="drum-pad" id="Heater 3">E<audio data-keys={69} src="../public/Heater-3.mp3" className="clip" id="E"></audio></button>
-      <button className="drum-pad" id="Heater 4">A<audio data-keys={65} src="../public/Heater-4_1.mp3" className="clip" id="A"></audio></button>
-      <button className="drum-pad" id="Clap">S<audio data-keys={83} src="../public/Heater-6.mp3" className="clip" id="S"></audio></button>
-      <button className="drum-pad" id="Open-HH">D<audio data-keys={68} src="../public/Dsc_Oh.mp3" className="clip" id="D"></audio></button>
-      <button className="drum-pad" id="Kick-n'-Hat">Z<audio data-keys={90} src="../public/Kick_n_Hat.mp3" className="clip" id="Z"></audio></button>
-      <button className="drum-pad" id="Kick">X<audio data-keys={88} src="../public/RP4_KICK_1.mp3" className="clip" id="X"></audio></button>
-      <button className="drum-pad" id="Closed-HH">C<audio data-keys={67} src="../public/Cev_H2.mp3" className="clip" id="C"></audio></button>
+      <button className="drum-pad" id="Heater 1">Q<audio data-keys={81} src="/Heater-1.mp3" className="clip" id="Q"></audio></button>
+      <button className="drum-pad" id="Heater 2">W<audio data-keys={87} src="/Heater-2.mp3" className="clip" id="W"></audio></button>
+      <button className="drum-pad" id="Heater 3">E<audio data-keys={69} src="/Heater-3.mp3" className="clip" id="E"></audio></button>
+      <button className="drum-pad" id="Heater 4">A<audio data-keys={65} src="/Heater-4_1.mp3" className="clip" id="A"></audio></button>
+      <button className="drum-pad" id="Clap">S<audio data-keys={83} src="/Heater-6.mp3" className="clip" id="S"></audio></button>
+      <button className="drum-pad" id="Open-HH">D<audio data-keys={68} src="/Dsc_Oh.mp3" className="clip" id="D"></audio></button>
+      <button className="drum-pad" id="Kick-n'-Hat">Z<audio data-keys={90} src="/Kick_n_Hat.mp3" className="clip" id="Z"></audio></button>
+      <button className="drum-pad" id="Kick">X<audio data-keys={88} src="/RP4_KICK_1.mp3" className="clip" id="X"></audio></button>
+      <button className="drum-pad" id="Closed-HH">C<audio data-keys={67} src="/Cev_H2.mp3" className="clip" id="C"></audio></button>
     </div>
     <div className="inner2">
       <i className="fa-solid fa-toggle-on fa-4x switch" id="knob"></i>
