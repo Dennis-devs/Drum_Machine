0afcd5660a65893b08bb51294941d33b1abe2a3f
Dennis-devs <dennis.ogoll@gmail.com>
2023-06-13 15:49:19 +0300

    Edit file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.mp3'],
   plugins: [react()],
-  base: "/Drum_Machine/docs/"
+  base: "/Drum_Machine/"
 })
